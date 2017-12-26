8d161f9e48c3a24652e2b7da49c8823060a0777d
sataylor8 <sataylor8@gmail.com>
2017-12-26 11:39:14 -0500

    Added song links

--- src/Components/ListItem.tsx
+++ src/Components/ListItem.tsx
@@ -12,11 +12,20 @@ export interface ListItemProps {
 
 export default class ListItem extends React.Component<ListItemProps, any> {
   render() {
-    let { Row1, Row2, Row3, ImageName } = this.props;
+    let { Row1, Row2, Row3, ImageName, SongUrl } = this.props;
+    let anchorStyle = {};
+    if (!SongUrl) {
+      anchorStyle = {
+        pointerEvents: 'none',
+        cursor: 'default',
+      }
+    }
     return (
       <div className="Container">
-        <div className="Divider"/>
-        <img className="Image" src={Data[ImageName]}/>
+        <div className="Divider"/>        
+        <a style={anchorStyle} target="_blank" href={SongUrl} className="ImageWrapper">
+          <img className="Image" src={Data[ImageName]}/>
+        </a>
         <h2 className="Row1"> {Row1} </h2>
         <h3 className="Row2"> {Row2} </h3>
         <h4 className="Row3"> {Row3} </h4>
